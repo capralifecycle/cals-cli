64b7bda548b5522ff56eab8c006d66a35a4387b6
Oyvind Moldestad <moldestad@gmail.com>
2019-04-04 12:01:58 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import yargs from 'yargs';
+
+console.log(`Argument is ${JSON.stringify(yargs.argv)}`);
+
+// Definer prosjekt-navn
+// Definer ønskede miljøer
+// Baselines som skal brukse
+// Er bruker logget inn?
+// Lag repo og endre commit
+// Osv. osv. osv.
