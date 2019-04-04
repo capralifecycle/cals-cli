8adddbcbbbe7ffccce243df7cd5ef9427e2b1bb2
Henrik Steen <henrist@henrist.net>
2019-04-04 12:51:54 +0200

    Adjust prettier settings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import yargs from 'yargs';
+import yargs from 'yargs'
 
 // tslint:disable-next-line
-console.log(`Argument is ${JSON.stringify(yargs.argv)}`);
+console.log(`Argument is ${JSON.stringify(yargs.argv)}`)
 
 // Definer prosjekt-navn
 // Definer ønskede miljøer
