12efb21da2cda4df1a7910e673d99e804974eeb5
Henrik Steen <henrist@henrist.net>
2019-04-07 21:31:42 +0200

    Initial work for building distribution package

---
+++ alias-build.sh
@@ -0,0 +1,9 @@
+# Build distribution:
+# $ npm run build
+# Source this file:
+# $ source alias-build.sh
+#
+# Then you can run the cli by only writing:
+# $ cals
+
+alias cals="node $PWD/build/cals-cli.js"
---
+++ cals-cli.js
@@ -0,0 +1,2 @@
+require('ts-node/register')
+require(__dirname + '/src/cli/index')
