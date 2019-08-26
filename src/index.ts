52b626ae02600e27371568b04ddacbdfb02d3f28
Henrik Steen <henrist@henrist.net>
2019-08-26 15:34:00 +0200

    feat: export Config and getDefinition as part of lib

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { version } from 'package.json'
+import { Config } from './config'
+import { getDefinition } from './definition/definition'
 
 export const VERSION = version
+
+export { Config, getDefinition }
