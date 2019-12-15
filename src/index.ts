f96b91157c5dcc0fb4fbfece95bc47e21bfb522d
Henrik Steen <henrist@henrist.net>
2019-12-15 14:28:03 +0100

    feat: export cache provider, github service and reporter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { version } from 'package.json'
+import { CacheProvider } from './cache'
+import { Reporter } from './cli/reporter'
+import { createReporter } from './cli/util'
 import { Config } from './config'
 import { getDefinition } from './definition/definition'
+import { createGitHubService, GitHubService } from './github/service'
 
 export const VERSION = version
 
-export { Config, getDefinition }
+export {
+  CacheProvider,
+  Config,
+  createGitHubService,
+  createReporter,
+  getDefinition,
+  GitHubService,
+  Reporter,
+}
