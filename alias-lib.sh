fa9156b1e5199d4677460e3e3fe263ba211cfb26
Henrik Steen <henrist@henrist.net>
2020-01-25 16:00:51 +0100

    chore: remove obsolete alias-lib script

--- alias-lib.sh
+++
@@ -1,9 +0,0 @@
-# Build distribution:
-# $ npm run build
-# Source this file:
-# $ source alias-lib.sh
-#
-# Then you can run the cli by only writing:
-# $ cals
-
-alias cals="node $PWD/lib/cals-cli.js"
