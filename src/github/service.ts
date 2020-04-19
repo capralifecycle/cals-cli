capralifecycle/cals-cli

commit f8a37280ad2ebc34b59d6113e0a2c66cf7789111

feat(cli): remove default for org argument
The org argument is now required for some github commands.

Also remove `all-orgs` argument of github configure command as
that will be the default when org is not specified.

src/github/service.ts
@@ -220,7 +220,7 @@ export class GitHubService {
     return (await response.json()) as T
   }
 
-  public async getRepoList({ owner }: { owner: string }) {
+  public async getOrgRepoList({ org }: { org: string }) {
     interface QueryResult {
       organization: {
         repositories: {
@@ -235,7 +235,7 @@ export class GitHubService {
     }
 
     const getQuery = (after: string | null) => `{
-  organization(login: "${owner}") {
+  organization(login: "${org}") {
     repositories(first: 100${after === null ? "" : `, after: "${after}"`}) {
       totalCount
       pageInfo {
@@ -268,7 +268,7 @@ export class GitHubService {
   }
 }`
 
-    return this.cache.json(`repos-${owner}`, async () => {
+    return this.cache.json(`repos-${org}`, async () => {
       const repos: Repo[] = []
       let after = null
 

src/cli/commands/definition/dump-setup.ts
@@ -51,7 +51,7 @@ async function getReposFromGitHub(
 ): Promise<DetailedProject["repos"][0]> {
   return (
     await pMap(orgs, async (org) => {
-      const repos = await github.getRepoList({ owner: org.login })
+      const repos = await github.getOrgRepoList({ org: org.login })
       return pMap(repos, async (repo) => {
         const detailedRepo = await github.getRepository(
           repo.owner.login,

src/cli/commands/github.ts
@@ -19,11 +19,6 @@ const command: CommandModule = {
       .command(listRepos)
       .command(listWebhooks)
       .command(setToken)
-      .options("org", {
-        default: "capralifecycle",
-        describe: "Specify GitHub organization",
-        type: "string",
-      })
       .demandCommand().usage(`cals github
 
 Notes:
@@ -33,13 +28,13 @@ Notes:
   $ cals github set-token
 
   Quick clone all repos:
-  $ cals github generate-clone-commands --all -x | bash
+  $ cals github generate-clone-commands --org capralifecycle --all -x | bash
 
   And for a specific project:
-  $ cals github generate-clone-commands -x buildtools | bash
+  $ cals github generate-clone-commands --org capralifecycle -x buildtools | bash
 
   Keeping up to date with removed/renamed repos:
-  $ cals github analyze-directory
+  $ cals github analyze-directory --org capralifecycle
 
   Some responses are cached for some time. Wipe the cals-cli
   cache folder in your users cache dir (~/.cache/cals-cli on Linux)

src/cli/commands/github/analyze-directory.ts
@@ -16,9 +16,9 @@ const analyzeDirectory = async (
   reporter: Reporter,
   config: Config,
   github: GitHubService,
-  owner: string,
+  org: string,
 ) => {
-  const repos = await github.getRepoList({ owner })
+  const repos = await github.getOrgRepoList({ org })
 
   const reposDict = repos.reduce<{
     [key: string]: Repo
@@ -75,6 +75,12 @@ const analyzeDirectory = async (
 const command: CommandModule = {
   command: "analyze-directory",
   describe: "Analyze directory for git repos",
+  builder: (yargs) =>
+    yargs.options("org", {
+      required: true,
+      describe: "Specify GitHub organization",
+      type: "string",
+    }),
   handler: async (argv) => {
     const config = createConfig()
     const github = await createGitHubService(

src/cli/commands/github/configure.ts
@@ -58,12 +58,12 @@ async function process(
   definition: Definition,
   getOrg: ReturnType<typeof createOrgGetter>,
   execute: boolean,
-  limitToOrg: string | null,
+  limitToOrg: string | undefined,
 ) {
   let changes: ChangeSetItem[] = []
 
   for (const orgName of getGitHubOrgs(definition)) {
-    if (limitToOrg !== null && limitToOrg !== orgName) {
+    if (limitToOrg !== undefined && limitToOrg !== orgName) {
       continue
     }
 
@@ -142,13 +142,11 @@ const command: CommandModule = {
         describe: "Execute the detected changes",
         type: "boolean",
       })
-      .options("all-orgs", {
-        describe: "Ignore organization filter",
-        type: "boolean",
+      .options("org", {
+        describe: "Filter resources by GitHub organization",
+        type: "string",
       }),
   handler: async (argv) => {
-    const org = !!argv["all-orgs"] ? null : (argv["org"] as string)
-
     const reporter = createReporter(argv)
     const config = createConfig()
     const github = await createGitHubService(
@@ -166,7 +164,7 @@ const command: CommandModule = {
         definition,
         orgGetter,
         !!argv["execute"],
-        org,
+        argv["org"] as string | undefined,
       )
     })
   },

src/cli/commands/github/generate-clone-commands.ts
@@ -16,7 +16,7 @@ const generateCloneCommands = async ({
   reporter,
   config,
   github,
-  owner,
+  org,
   ...opt
 }: {
   reporter: Reporter
@@ -29,14 +29,14 @@ const generateCloneCommands = async ({
   listGroups: boolean
   name: string | undefined
   topic: string | undefined
-  owner: string
+  org: string
 }) => {
   if (!opt.listGroups && !opt.all && opt.group === undefined) {
     yargs.showHelp()
     return
   }
 
-  const repos = await github.getRepoList({ owner })
+  const repos = await github.getOrgRepoList({ org })
   const groups = getGroupedRepos(repos)
 
   if (opt.listGroups) {
@@ -72,11 +72,16 @@ const generateCloneCommands = async ({
 
 const command: CommandModule = {
   command: "generate-clone-commands",
-  describe: "Generate shell commands to clone CALS Git repos",
+  describe: "Generate shell commands to clone GitHub repos for an organization",
   builder: (yargs) =>
     yargs
       .positional("group", {
         describe: "Group to generate commands for",
+      })
+      .options("org", {
+        required: true,
+        describe: "Specify GitHub organization",
+        type: "string",
       })
       .option("all", {
         describe: "Use all groups",
@@ -123,7 +128,7 @@ const command: CommandModule = {
       topic: argv.topic as string | undefined,
       excludeExisting: !!argv["exclude-existing"],
       group: argv.group as string | undefined,
-      owner: argv["org"] as string,
+      org: argv["org"] as string,
     })
   },
 }

src/cli/commands/github/list-repos.ts
@@ -28,7 +28,7 @@ const listRepos = async ({
   topic = undefined,
   compact,
   csv,
-  owner,
+  org,
 }: {
   reporter: Reporter
   github: GitHubService
@@ -37,9 +37,9 @@ const listRepos = async ({
   topic?: string
   compact: boolean
   csv: boolean
-  owner: string
+  org: string
 }) => {
-  let repos = await github.getRepoList({ owner })
+  let repos = await github.getOrgRepoList({ org })
 
   if (!includeArchived) {
     repos = repos.filter((it) => !it.isArchived)
@@ -130,9 +130,14 @@ const listRepos = async ({
 
 const command: CommandModule = {
   command: "list-repos",
-  describe: "List CALS Git repos",
+  describe: "List Git repos for a GitHub organization",
   builder: (yargs) =>
     yargs
+      .options("org", {
+        required: true,
+        describe: "Specify GitHub organization",
+        type: "string",
+      })
       .option("include-archived", {
         alias: "a",
         describe: "Include archived repos",
@@ -169,7 +174,7 @@ const command: CommandModule = {
       topic: argv.topic as string | undefined,
       compact: !!argv.compact,
       csv: !!argv.csv,
-      owner: argv["org"] as string,
+      org: argv["org"] as string,
     })
   },
 }

src/cli/commands/github/list-webhooks.ts
@@ -26,9 +26,9 @@ const listWebhooks = async (
   reporter: Reporter,
   cache: CacheProvider,
   github: GitHubService,
-  owner: string,
+  org: string,
 ) => {
-  const repos = (await github.getRepoList({ owner })).filter(
+  const repos = (await github.getOrgRepoList({ org })).filter(
     (it) => !it.isArchived,
   )
 
@@ -98,7 +98,13 @@ const listWebhooks = async (
 
 const command: CommandModule = {
   command: "list-webhooks",
-  describe: "List CALS Git repos web hooks",
+  describe: "List webhooks for repositories in for a GitHub organization",
+  builder: (yargs) =>
+    yargs.options("org", {
+      required: true,
+      describe: "Specify GitHub organization",
+      type: "string",
+    }),
   handler: async (argv) => {
     const config = createConfig()
     const cacheProvider = createCacheProvider(config, argv)

commit 1fd50ecbd0c84f81aaaecd7c38f03906aec188a9

fix: restore getRepoList to avoid breaking change
The breaking change was introduced in
f8a37280ad2ebc34b59d6113e0a2c66cf7789111

src/github/service.ts
@@ -193,6 +193,13 @@ export class GitHubService {
     return json.data
   }
 
+  /**
+   * @deprecated
+   */
+  public async getRepoList({ owner }: { owner: string }) {
+    return this.getOrgRepoList({ org: owner })
+  }
+
   public async getOrgRepoList({ org }: { org: string }) {
     interface QueryResult {
       organization: {