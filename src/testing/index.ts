9bccbbbfc0eebcc1886bf846687426dede59a253
Henrik Steen <henrist@henrist.net>
2020-08-31 16:08:39 +0200

    feat: initial utils for e2e testing (#78)

---
+++ src/testing/index.ts
@@ -0,0 +1,10 @@
+export { createTestExecutor, TestExecutor } from "./executor"
+export {
+  createNetwork,
+  curl,
+  pollForCondition,
+  runNpmRunScript,
+  startContainer,
+  waitForHttpOk,
+  waitForPostgresAvailable,
+} from "./lib"
