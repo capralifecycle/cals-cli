4c16d8eb753ed9d8d7f9b7fa1a255914974e4abf
Henrik Steen <henrist@henrist.net>
2020-11-02 12:20:07 +0100

    refactor: exports

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { CacheProvider } from "./cache"
 import { Reporter } from "./cli/reporter"
 import { createReporter } from "./cli/util"
 import { Config } from "./config"
-import * as definition from "./definition"
 import { DefinitionFile } from "./definition/definition"
-import * as github from "./github"
 import { createGitHubService, GitHubService } from "./github/service"
 
 export const VERSION = version
 
+export * as definition from "./definition"
+export * as github from "./github"
 export * as snyk from "./snyk"
 export * from "./testing"
 // Consider removing old exports later.
@@ -19,8 +19,6 @@ export {
   createGitHubService,
   createReporter,
   DefinitionFile,
-  definition,
-  github,
   GitHubService,
   Reporter,
 }
