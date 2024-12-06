2d22de06a6bbe20f42c6f2be4bf1de38ec3ba831
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-12-06 14:52:01 +0100

    feat: migrate primary project config to esm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,27 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default [{
-    ignores: ["coverage/", "lib/", "**/*.js", "**/*.mjs"],
-}, ...compat.extends(
+export default [
+  {
+    ignores: ["coverage/", "lib/", "**/*.js", "**/*.mjs", "scripts/**/*"],
+  },
+  ...compat.extends(
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
     "plugin:prettier/recommended",
-), {
+  ),
+  {
     files: ["**/*.ts"],
     languageOptions: {
       ecmaVersion: 5,
@@ -30,4 +33,5 @@ export default [{
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
     },
-}];
+  },
+]
